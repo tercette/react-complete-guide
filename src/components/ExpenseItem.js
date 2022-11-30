bc48c94fb5f1fea5cb6cda9d534d631087c4c6d8
leandro_tercette <leandro_tercette@gmail.com>
2022-11-30 18:10:40 -0300

    implemented concept of composition

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,16 @@
+import ExpenseDate from './ExpenseDate'
+import Card from './Card';
 import './ExpenseItem.css';
 
-function ExpenseItem() {
-const expenseDate = new Date();
-const expenseTitle = 'Car Insurance';
-const expenseAmount = 294.67;
-
+function ExpenseItem(props) {
     return (
-        <div className='expense-item'>
-            <div className='date' >{expenseDate.toLocaleDateString("pt-BR")} </div>
+        <Card className='expense-item'>
+            <ExpenseDate date={props.date}/>
             <div className='expense-item__description'>
-                <h2>{expenseTitle}</h2>
-                <div className='expense-item__price'>${expenseAmount}</div>
+                <h2>{props.title}</h2>
+                <div className='expense-item__price'>${props.amount}</div>
             </div>
-        </div>
+        </Card>
     );
 }
 
