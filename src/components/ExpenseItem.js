e70d3b31d89e6175a6d596b6358d3d3ba4771a65
leandro_tercette <leandro_tercette@gmail.com>
2022-11-30 12:10:21 -0300

    First component created

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,19 @@
+import './ExpenseItem.css';
+
+function ExpenseItem() {
+const expenseDate = new Date();
+const expenseTitle = 'Car Insurance';
+const expenseAmount = 294.67;
+
+    return (
+        <div className='expense-item'>
+            <div className='date' >{expenseDate.toLocaleDateString("pt-BR")} </div>
+            <div className='expense-item__description'>
+                <h2>{expenseTitle}</h2>
+                <div className='expense-item__price'>${expenseAmount}</div>
+            </div>
+        </div>
+    );
+}
+
+export default ExpenseItem;
