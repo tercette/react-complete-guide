cf9cdd414d403560770a2d3f8f4a595ecfdf01cc
leandro_tercette <leandro_tercette@gmail.com>
2022-12-01 12:06:06 -0300

    lifting the state up

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 const App = () => {
   const expenses = [
@@ -25,9 +26,14 @@ const App = () => {
     },
   ];
 
+  const addExpenseHandler = expense => {
+    console.log('In App.js')
+    console.log(expense)
+  }
+
   return (
     <div className="App">
-      <h2>Lets get started!</h2>
+      <NewExpense onAddExpence = {addExpenseHandler}/>
       <Expenses items={expenses} />
     </div>
   );
