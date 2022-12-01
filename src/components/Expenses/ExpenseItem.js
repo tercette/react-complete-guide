fc0e68c702d6b889f1ea51726de37e51ae55b828
leandro_tercette <leandro_tercette@gmail.com>
2022-12-01 17:53:00 -0300

    clean code

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,25 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ExpenseDate from './ExpenseDate'
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
-const [title, setTitle] = useState(props.title); //1 posicao eh a variavel e a segunda é a funcao que atualiza
-const [id, setid] = useState();
-
-const clickHandler = () => {
-    setTitle('Updated!');
-    setid('change-id');
-    }
-
     return (
         <Card className='expense-item'>
             <ExpenseDate date={props.date}/>
             <div className='expense-item__description'>
-                <h2  id={id}>{title}</h2>
+                <h2>{props.title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
             </div>
-                <button onClick={clickHandler} type="button">Change Title</button>
         </Card>
     );
 }
